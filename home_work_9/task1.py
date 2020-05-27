b9e651b102c3d048c5975f10d249b6ee36fafa4d
JaneDoe <ilyaburtseff@mail.ru>
2020-05-27 17:31:32 +0300

    debug

--- home_work_9/task1.py
+++ home_work_9/task1.py
@@ -7,25 +7,20 @@
 #
 # * без использования функций для вычисления хэша (hash(),
 # sha1() или любой другой из модуля hashlib задача считается не решённой.
+
+# №1 написать функцию которая на вход принимает строку анализирует
+# ее и возвращает целое число(количество подстрок в данной строке)
 import hashlib
 
 
 def counter(data):
     if data.count(' ') > 0:
         total = []
-        lst_sorted = []
         result = data.split(' ')
         for itm in result:
             total.append(hashlib.sha1(itm.encode('utf - 8')).hexdigest())
-        total = sorted(total)
-        for itm in range(len(total) - 1):
-            if total[itm] == total[itm + 1]:
-                total[itm] = 0
-        for itm in total:
-            if itm != 0:
-                lst_sorted.append(itm)
-
-        return lst_sorted
+        total = set(sorted(total))
+        return total
 
     else:
         new_len = len(data)
@@ -36,36 +31,20 @@ def counter(data):
             long = zip(*args)
             container = [''.join(itm) for itm in long]
             total_lst = []
-            finally_lst = []
             for itm in container:
                 total_lst.append(hashlib.sha1(itm.encode('utf-8')).hexdigest())
-            total_lst = sorted(total_lst)
-            for itm in range(len(total_lst) - 1):
-                if total_lst[itm] == total_lst[itm + 1]:
-                    total_lst[itm] = 0
-
-            for itm in total_lst:
-                if itm != 0:
-                    finally_lst.append(itm)
-            return finally_lst
+            total_lst = set(sorted(total_lst))
+            return total_lst
         if new_len & 1:
             n = 3
             args = [iter(data)] * n
             long = zip(*args)
             container = [''.join(itm) for itm in long]
             total_lst = []
-            finally_lst = []
             for itm in container:
                 total_lst.append(hashlib.sha1(itm.encode('utf-8')).hexdigest())
-            total_lst = sorted(total_lst)
-            for itm in range(len(total_lst) - 1):
-                if total_lst[itm] == total_lst[itm + 1]:
-                    total_lst[itm] = 0
-
-            for itm in total_lst:
-                if itm != 0:
-                    finally_lst.append(itm)
-            return finally_lst
+            total_lst = set(sorted(total_lst))
+            return total_lst
 
 
 dat = input('Введите строку: \n')
